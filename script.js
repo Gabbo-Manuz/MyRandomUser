061e1914052fb55297a7fce5aad1681c76440d4d
Gabbo-Manuz <g.manunta.3560@vallauri.edu>
2025-12-17 22:10:12 +0100

    Commit

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-"use strict"
+"use strict";
+
+let params = {
+  results: 20,
+  gender: "male",
+  nat: "US"
+};
+let promise = ajax.sendRequest("GET", "/api", params);
+promise.then(function (results) {
+  console.log(results);
+});
