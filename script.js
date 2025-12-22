cb7a045ff6102c193f4b57832b665f7f6577e711
Gabbo-Manuz <g.manunta.3560@vallauri.edu>
2025-12-23 00:13:38 +0100

    Commit

--- script.js
+++ script.js
@@ -1,11 +1,45 @@
 "use strict";
 
 let params = {
+  seed: "81f573bee181a743",
   results: 20,
-  gender: "male",
-  nat: "US"
 };
-let promise = ajax.sendRequest("GET", "/api", params);
-promise.then(function (results) {
-  console.log(results);
+
+$(document).on("click",".card" ,function () {
+  console.log("dd")
 });
+
+
+loadCard(params);
+
+async function loadCard(params) {
+  let HTTPResponse = await loadData(params);
+  console.log(HTTPResponse);
+  HTTPResponse.data.results.forEach(function (person, i) {
+    $("#boxCardScrol").append(createCard(person));
+  });
+}
+
+function createCard(person) {
+  return $("<div/>")
+    .addClass("card")
+    .append(
+      $("<div/>")
+        .css({
+          width: "70px",
+          height: "70px",
+          margin: "5px",
+          backgroundImage: `url(${person.picture.medium})`,
+          backgroundPosition: "center",
+          border: "2px,solid,#2D6A4F",
+        })
+        .addClass("rounded-circle"),
+      $("<div/>")
+        .text(`${person.name.first} ${person.name.last}`)
+        .css({ margin: "5px" })
+    );
+}
+
+async function loadData(params) {
+  return await ajax.sendRequest("GET", "./api", params);
+}
